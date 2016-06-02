56f3e6f2a1e15bd4659cddf44a0d07475db4b107
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-06-01 22:25:34 -0300

    Split js files

--- app/assets/javascript/app.js
+++ app/assets/javascript/app.js
@@ -1,54 +1,4 @@
-angular.module('flapperNews', ['ui.router'])
-.controller('MainCtrl', [
-  '$scope',
-  '$stateParams',
-  'posts',
-  function($scope,$stateParams, posts){
-    $scope.test = 'Hello world!';
-    $scope.posts = posts.posts;
-    
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0,
-        comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
-  }]);
-angular.module('flapperNews')
-.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', 
-  function($scope,$stateParams, posts){
-    $scope.post = posts.posts[$stateParams.id];
-    $scope.addComment = function(){
-      if($scope.body === '') { return; }
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = '';
-    };
-}])
-
-angular.module('flapperNews')
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-
-angular.module('flapperNews')
+angular.module('flapperNews', ['ui.router', 'templates'])
 .config([
   '$stateProvider',
   '$urlRouterProvider',
@@ -57,12 +7,12 @@ angular.module('flapperNews')
     $stateProvider
     .state('home', {
       url: '/home',
-      templateUrl: '/home.html',
+      templateUrl: 'home/_home.html',
       controller: 'MainCtrl'
     })
     .state('posts', {
       url: '/posts/{id}',
-      templateUrl: '/posts.html',
+      templateUrl: 'posts/_posts.html',
       controller: 'PostsCtrl'
     });
 
